63ff2d0c3feab3d30099231ae993731cc6730d24
maurebi <meschelhaas@LAPTOP-V8OKQ15T.localdomain>
2025-03-10 14:04:17 +0100

    Commit main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Machine Learning Project
+# Nikki van Gurp, Ilse Kerkhove, Dertje Roggeveen & Marieke Schelhaas
+
+
+def main():
+    print('Initial')
+    
+
+
+if __name__ == "__main__":
+    main()

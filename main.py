33fb82f32d21135a54a7e1dc15af680b57a40601
maurebi <meschelhaas@LAPTOP-V8OKQ15T.localdomain>
2025-03-19 12:29:12 +0100

    Commit to pull

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 # Nikki van Gurp, Ilse Kerkhove, Dertje Roggeveen & Marieke Schelhaas
 
 
+def read_dataset(subset, split):
+    pass
+
+
+def preprocess_dataset(text_list):
+    pass
+
+
 def main():
     print('Initial')
     
-
 
 if __name__ == "__main__":
     main()

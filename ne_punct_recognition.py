2746c5feae4e997828ce2dcd22e6f0ea1aa21282
maurebi <meschelhaas@LAPTOP-V8OKQ15T.localdomain>
2025-04-04 09:40:26 +0200

    Forgot to add ne_punct_recognition.py

---
+++ ne_punct_recognition.py
@@ -0,0 +1,30 @@
+import spacy
+import string
+
+
+def ne_punct_recognizer(words, tweet_num, tweets):
+    print(f"- Processing Named Entities and Punctuation...")
+    labels = []
+    currentTweet = None
+    nlpEN = spacy.load('en_core_web_sm') 
+    punctuation_set = set(string.punctuation)
+
+    for i in range(len(words)): 
+        # Keep in mind what tweet we are
+        if tweet_num[i] != currentTweet:
+            currentTweet = str(tweets[int(tweet_num[i])])
+            doc = nlpEN(currentTweet)
+            entity_words = {ent.text for ent in doc.ents}
+        
+        if words[i] in punctuation_set:
+            labels.append('other')
+            continue
+        
+        if entity_words and any(words[i] in ent.split() for ent in entity_words):
+            labels.append('ne')
+        else:
+            labels.append('none')
+    print(f"- Finished processing Named Entities and Punctuation...") 
+
+
+    return labels

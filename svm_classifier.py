cc321126707dd9d6e2a29b0f7cee295c346c415c
DertjeR <d.j.roggeveen@student.rug.nl>
2025-04-04 15:20:06 +0200

    added a possible improvement in the init, but takes a lot of time

--- svm_classifier.py
+++ svm_classifier.py
@@ -9,10 +9,13 @@ class SVMClassifier(CustomClassifier):
     def __init__(self, kernel='linear'):
         super().__init__()
         self.counter = None
-        self.classifier = OneVsOneClassifier(svm.LinearSVC(class_weight='balanced', dual=False, max_iter=1000))
+        self.classifier = OneVsOneClassifier(svm.LinearSVC(class_weight='balanced', dual=False, max_iter=5000))
+        # self.classifier = OneVsOneClassifier(svm.SVC(kernel='rbf', class_weight='balanced', probability=True)) # Might be better but takes very long)
 
     def fit(self, train_features, train_labels):
+        print("Fitting model...")
         self.classifier.fit(train_features, train_labels)
 
     def predict(self, test_features):
+        print("Predicting...")
         return self.classifier.predict(test_features)

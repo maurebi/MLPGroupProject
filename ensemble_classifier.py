06f0ada0d6fbdc2b35c0c744c6b9012e14ea2b8b
DertjeR <d.j.roggeveen@student.rug.nl>
2025-04-04 15:21:03 +0200

    improved stacking by changing self.ensemble

--- ensemble_classifier.py
+++ ensemble_classifier.py
@@ -2,8 +2,10 @@ from custom_classifier import CustomClassifier
 from svm_classifier import SVMClassifier
 from naive_bayes import NaiveBayesClassifier
 from knn import KNNClassifier
-from sklearn.ensemble import VotingClassifier, StackingClassifier
+from sklearn.ensemble import VotingClassifier, StackingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import ComplementNB
 
 #TODO update requirements.txt
 
@@ -46,14 +48,31 @@ class StackingEnsembleClassifier(CustomClassifier):
         self.nb = NaiveBayesClassifier()
         self.knn = KNNClassifier()
 
+        # self.ensemble = StackingClassifier(
+        #     estimators=[
+        #         ('svm', self.svm.classifier),
+        #         ('naive_bayes', self.nb.model),
+        #         ('knn', self.knn.knn)
+        #     ],
+        #     final_estimator=LogisticRegression(),
+        #     passthrough=True
+        # )
         self.ensemble = StackingClassifier(
             estimators=[
                 ('svm', self.svm.classifier),
                 ('naive_bayes', self.nb.model),
                 ('knn', self.knn.knn)
             ],
-            final_estimator=LogisticRegression(),
-            passthrough=True
+            final_estimator=RandomForestClassifier(
+                n_estimators=100, 
+                max_depth=10, 
+                min_samples_split=5, 
+                class_weight='balanced', 
+                random_state=42
+            ),
+            cv=5,  # Use 5-fold cross-validation for out-of-fold predictions
+            passthrough=True,  # Pass original features to the meta-classifier
+            n_jobs=-1  # Parallelize training
         )
 
     def fit(self, train_labels, train_text, tweet_num, tweets):

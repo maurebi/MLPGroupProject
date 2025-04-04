f6bd51da093032d67caf56e7a3c3b6739dbb5a01
DertjeR <d.j.roggeveen@student.rug.nl>
2025-04-04 12:46:38 +0200

    moved functions from ensemble classifiers to custom classifier to remove redundant code

--- custom_classifier.py
+++ custom_classifier.py
@@ -1,12 +1,16 @@
 import abc
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-
+import numpy as np
+import string
+from ne_punct_recognition import ne_punct_recognizer
+from nltk.corpus import words, stopwords
 
 class CustomClassifier(abc.ABC):
     def __init__(self):
         self.counter = None
+        self.english_words = set(words.words())
+        self.spanish_stopwords = set(stopwords.words('spanish'))
 
-    
     def get_features(self, text_list):
         """ Return word (or ngram) count features for each text as a 2D numpy array """
         if self.counter is None:
@@ -17,11 +21,60 @@ class CustomClassifier(abc.ABC):
 
         return features_array
 
-
     def tf_idf(self, text_feats):
         tfidf_transformer = TfidfTransformer().fit(text_feats)
         return tfidf_transformer.transform(text_feats)
-    
+
+    def extract_handcrafted_features(self, word_list):
+        features = []
+        for word in word_list:
+            word_lower = word.lower()
+            has_accent = any(c in "áéíóúñ" for c in word_lower)
+            is_ascii = all(ord(c) < 128 for c in word_lower)
+            is_capitalized = word[0].isupper() if word else False
+            only_special_chars = all(c in string.punctuation for c in word)
+            contains_numbers = any(c.isdigit() for c in word)
+            avg_unicode = np.mean([ord(c) for c in word_lower]) if word else 0
+
+            in_english_dict = int(word_lower in self.english_words)
+            in_spanish_dict = int(word_lower in self.spanish_stopwords)
+            in_both_dicts = int(in_english_dict and in_spanish_dict)
+            in_neither = int(not in_english_dict and not in_spanish_dict)
+
+            features.append([
+                int(has_accent),
+                int(is_ascii),
+                int(is_capitalized),
+                int(only_special_chars),
+                int(contains_numbers),
+                avg_unicode,
+                in_english_dict,
+                in_spanish_dict,
+                in_both_dicts,
+                in_neither,
+            ])
+        return np.array(features)
+
+    def get_punctuation_features(self, punct_labels):
+        punct_features = [1 if label == 'other' else 0 for label in punct_labels]
+        return np.array(punct_features).reshape(-1, 1)
+
+    def get_named_entity_features(self, ne_labels):
+        ne_features = [1 if label == 'ne' else 0 for label in ne_labels]
+        return np.array(ne_features).reshape(-1, 1)
+
+    def get_combined_features(self, text, tweet_num, tweets):
+        text_tfidf = self.tf_idf(self.get_features(text))
+        text_tfidf = np.array(text_tfidf.toarray())
+
+        ne_punct_labels = ne_punct_recognizer(text, tweet_num, tweets)
+        punct_features = self.get_punctuation_features(ne_punct_labels)
+        ne_features = self.get_named_entity_features(ne_punct_labels)
+
+        handcrafted_feats = self.extract_handcrafted_features(text)
+
+        combined_feats = np.hstack([text_tfidf, punct_features, ne_features, handcrafted_feats])
+        return combined_feats
 
     @abc.abstractmethod
     def fit(self, train_features, train_labels):
--- ensemble_classifier.py
+++ ensemble_classifier.py
@@ -2,13 +2,8 @@ from custom_classifier import CustomClassifier
 from svm_classifier import SVMClassifier
 from naive_bayes import NaiveBayesClassifier
 from knn import KNNClassifier
-from ne_punct_recognition import ne_punct_recognizer
 from sklearn.ensemble import VotingClassifier, StackingClassifier
 from sklearn.linear_model import LogisticRegression
-import numpy as np
-import nltk
-from nltk.corpus import words, stopwords
-import string
 
 #TODO update requirements.txt
 
@@ -19,8 +14,6 @@ class VotingEnsembleClassifier(CustomClassifier):
         self.svm = SVMClassifier()
         self.nb = NaiveBayesClassifier()
         self.knn = KNNClassifier()
-        self.english_words = set(words.words())
-        self.spanish_stopwords = set(stopwords.words('spanish'))
 
         self.ensemble = VotingClassifier(
             estimators=[
@@ -32,95 +25,19 @@ class VotingEnsembleClassifier(CustomClassifier):
         )
 
     def fit(self, train_labels, train_text, tweet_num, tweets):
-        print("- Generating features...")
         combined_train_features = self.get_combined_features(train_text, tweet_num, tweets)
-        
-        # fit all individual classifiers
-        print("- Fitting individual classifiers...")
+
+        # Fit individual classifiers
         self.svm.fit(combined_train_features, train_labels)
         self.nb.fit(combined_train_features, train_labels)
         self.knn.fit(combined_train_features, train_labels)
 
-        # fit the ensemble
-        print("- Fitting ensemble classifier...")
+        # Fit ensemble
         self.ensemble.fit(combined_train_features, train_labels)
 
     def predict(self, test_text, tweet_num, tweets):
         combined_test_features = self.get_combined_features(test_text, tweet_num, tweets)
-
         return self.ensemble.predict(combined_test_features)
-
-    def extract_handcrafted_features(self, word_list):
-        print("Extracting handcrafted features...")
-        features = []
-        words_left = len(word_list)
-
-        for word in word_list:
-            print(f"Number of words left is {words_left}")
-            word_lower = word.lower()
-            has_accent = any(c in "áéíóúñ" for c in word_lower)
-            is_ascii = all(ord(c) < 128 for c in word_lower)
-            is_capitalized = word[0].isupper() if word else False
-            only_special_chars = all(c in string.punctuation for c in word)
-            contains_numbers = any(c.isdigit() for c in word)
-            avg_unicode = np.mean([ord(c) for c in word_lower]) if word else 0
-            
-            in_english_dict = int(word_lower in self.english_words)
-            in_spanish_dict = int(word_lower in self.spanish_stopwords)
-            in_both_dicts = int(in_english_dict and in_spanish_dict)
-            in_neither = int(not in_english_dict and not in_spanish_dict)
-
-            features.append([
-                int(has_accent),
-                int(is_ascii),
-                int(is_capitalized),
-                int(only_special_chars),
-                int(contains_numbers),
-                avg_unicode,
-                in_english_dict,
-                in_spanish_dict,
-                in_both_dicts,
-                in_neither,
-            ])
-        
-            words_left = words_left - 1
-        
-        print("Finished getting handcrafted features.")
-        
-        return np.array(features)
-    
-    def get_combined_features(self, train_text, tweet_num, tweets):
-        print("Getting combined features...")
-        text_tfidf = self.tf_idf(self.get_features(train_text))
-        text_tfidf = np.array(text_tfidf.toarray())
-        
-        ne_punct_labels = ne_punct_recognizer(train_text, tweet_num, tweets)     
-        punct_features = self.get_punctuation_features(ne_punct_labels)
-        ne_features = self.get_named_entity_features(ne_punct_labels)
-        
-        handcrafted_feats = self.extract_handcrafted_features(train_text)
-
-        combined_feats = np.hstack([text_tfidf, punct_features, ne_features, handcrafted_feats])
-        print("Finished getting combined features...")
-
-        return combined_feats
-
-
-    def get_punctuation_features(self, punct_labels):
-        print("Getting punctuation features...")
-        punct_features = [1 if label == 'other' else 0 for label in punct_labels]
-        print("Finished getting punctuation features.") 
-
-        return np.array(punct_features).reshape(-1, 1)  
-    
-    def get_named_entity_features(self, ne_labels):
-        print("Getting named entity features...")
-        ne_features = [1 if label == 'ne' else 0 for label in ne_labels]
-        print("Finished getting named entity features.")
-        
-        return np.array(ne_features).reshape(-1, 1)
-
-
 
 class StackingEnsembleClassifier(CustomClassifier):
     def __init__(self):
@@ -128,8 +45,6 @@ class StackingEnsembleClassifier(CustomClassifier):
         self.svm = SVMClassifier()
         self.nb = NaiveBayesClassifier()
         self.knn = KNNClassifier()
-        self.english_words = set(words.words())
-        self.spanish_stopwords = set(stopwords.words('spanish'))
 
         self.ensemble = StackingClassifier(
             estimators=[
@@ -142,77 +57,14 @@ class StackingEnsembleClassifier(CustomClassifier):
         )
 
     def fit(self, train_labels, train_text, tweet_num, tweets):
-        print("- Generating features...")
         combined_train_features = self.get_combined_features(train_text, tweet_num, tweets)
-        
-        # fit all individual classifiers
-        print("- Fitting individual classifiers...")
+
         self.svm.fit(combined_train_features, train_labels)
         self.nb.fit(combined_train_features, train_labels)
         self.knn.fit(combined_train_features, train_labels)
 
-        # fit the ensemble
-        print("- Fitting ensemble classifier...")
         self.ensemble.fit(combined_train_features, train_labels)
 
     def predict(self, test_text, tweet_num, tweets):
         combined_test_features = self.get_combined_features(test_text, tweet_num, tweets)
-
         return self.ensemble.predict(combined_test_features)
-
-    def extract_handcrafted_features(self, word_list):
-        features = []
-
-        for word in word_list:
-            word_lower = word.lower()
-            has_accent = any(c in "áéíóúñ" for c in word_lower)
-            is_ascii = all(ord(c) < 128 for c in word_lower)
-            is_capitalized = word[0].isupper() if word else False
-            only_special_chars = all(c in string.punctuation for c in word)
-            contains_numbers = any(c.isdigit() for c in word)
-            avg_unicode = np.mean([ord(c) for c in word_lower]) if word else 0
-
-            in_english_dict = int(word_lower in self.english_words)
-            in_spanish_dict = int(word_lower in self.spanish_stopwords)
-            in_both_dicts = int(in_english_dict and in_spanish_dict)
-            in_neither = int(not in_english_dict and not in_spanish_dict)
-
-            features.append([
-                int(has_accent),
-                int(is_ascii),
-                int(is_capitalized),
-                int(only_special_chars),
-                int(contains_numbers),
-                avg_unicode,
-                in_english_dict,
-                in_spanish_dict,
-                in_both_dicts,
-                in_neither,
-            ])
-        
-        return np.array(features)
-
-    def get_combined_features(self, train_text, tweet_num, tweets):
-        text_tfidf = self.tf_idf(self.get_features(train_text))
-        text_tfidf = np.array(text_tfidf.toarray())
-        
-        ne_punct_labels = ne_punct_recognizer(train_text, tweet_num, tweets)     
-        punct_features = self.get_punctuation_features(ne_punct_labels)
-        ne_features = self.get_named_entity_features(ne_punct_labels)
-        
-        handcrafted_feats = self.extract_handcrafted_features(train_text)
-
-        combined_feats = np.hstack([text_tfidf, punct_features, ne_features, handcrafted_feats])
-
-        return combined_feats
-
-
-    def get_punctuation_features(self, punct_labels):   
-        punct_features = [1 if label == 'other' else 0 for label in punct_labels]  
-
-        return np.array(punct_features).reshape(-1, 1)  
-    
-    def get_named_entity_features(self, ne_labels):
-        ne_features = [1 if label == 'ne' else 0 for label in ne_labels]
-        
-        return np.array(ne_features).reshape(-1, 1)
